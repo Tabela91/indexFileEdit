5db92491705f26bf7c83c3ff6f4c9b27edb3b22c
Tabela91 <sethabela91@gmail.com>
2016-03-08 11:59:12 +0100

    Added Site resources
    Adding files necessary to run the website

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>GitHub Edit Test</h1>
+
+<img src="photo1.png" width ="100"/>
+<img src="photo2.png" width ="100"/>
+<img src="photo3.png" width ="100"/>
+<img src="photo4.png" width ="100"/>
+<img src="photo5.png" width ="100"/>
